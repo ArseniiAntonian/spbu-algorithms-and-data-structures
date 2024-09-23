e36fc287051b99e575630f88efa237b0ee8498b0
Arsenii Antonian <zvnlxn@gmail.com>
2024-09-23 14:26:42 +0300

    fixes

--- assignment1/main.py
+++ assignment1/main.py
@@ -20,29 +20,27 @@ def generate_data(n, pm_sys_weights, bank_weights):
     return data
 
 if __name__ == "__main__": 
-
-    start_time = time.time()
-
+    payment_systems = ['МИР', 'MasterCard', 'Visa']
+    banks = ['Сбер', 'Т-банк', 'ВТБ', 'Альфа банк']
     pm_sys_weights = []
     bank_weights = []
 
-    print("Введите веса для платежных систем 'МИР', 'MasterCard', 'Visa'")
-
     for i in range(3):
+        print(f"Введите вес для платежной системы {payment_systems[i]}: ")
         weight = float(input())
         pm_sys_weights.append(weight)
 
-    print("Введите веса для банков 'Сбер', 'Т-банк', 'ВТБ', 'Альфа банк'")
-
     for i in range(4):
+        print(f"Введите вес для банка {banks[i]}: ")
         weight = float(input())
         bank_weights.append(weight)
     
+    start_time = time.time()
     data = generate_data(100000, pm_sys_weights, bank_weights)
     df = pd.DataFrame(data)
     end_time = time.time()
 
     
-    df.to_csv('assignment1\purchases.csv', index=False, encoding='utf-8')
+    df.to_xml('assignment1/purchases.xml', index=False, encoding='utf-8', root_name='purchases', row_name='purchase')
     print(f"Сгенерирован за {(end_time - start_time):.2f}")
     print(df.head(10))
