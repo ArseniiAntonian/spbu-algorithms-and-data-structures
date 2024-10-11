0f3726349df9ac4176cdb8e241dace2619eb9046
Arsenii Antonian <zvnlxn@gmail.com>
2024-10-11 12:41:07 +0300

    add assignment2

--- assignment2/main.py
+++ assignment2/main.py
@@ -3,9 +3,6 @@ import functions as fun
 
 df = pd.read_xml("assignment1/purchases.xml")
 
-print(f"K-anonymity до обезличивания: {fun.calculate_k_anonymity(df, list(df.columns), 1)}")
-quasies = []
-
 if input('Обезличить название магазина? [y/n]') == 'y':
     df = fun.shop_depersonalization(df)
 if input('Обезличить координаты магазина? [y/n]') == 'y':
@@ -24,11 +21,5 @@ if input('Обезличить стоимость покупки? [y/n]') == 'y'
     df = fun.cost_depersonalization(df)
 
 print(df.head(10))
-df = fun.remove_unique_rows(df)
-k_values = fun.calculate_k_anonymity(df, list(df.columns), 5)
-print(f"K-anonymity после обезличивания : {k_values}")
-# total_records = len(df)
-# percentages = [(k / total_records) * 100 for k in k_values]
-# print("\nПроцентное соотношение первых K-анонимностей:")
-# print(percentages)
+fun.calculate_k_anonymity(df, list(df.columns), 5)
 df.to_xml('assignment2/depersonalized_purchases.xml', index=False, encoding='utf-8', root_name='purchases', row_name='purchase')
